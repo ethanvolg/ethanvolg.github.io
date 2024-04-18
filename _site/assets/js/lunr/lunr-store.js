44dd0d04e378b2473f85d43f36ad84a2e0552028
ethanvolg <ljj011124@gmail.com>
2024-04-18 12:53:36 +0900

    수정본

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,7 @@
 var store = [{
         "title": "첫 포스팅 입니다!",
-        "excerpt":"오늘 처음입니다.     ","categories": [],
+        "excerpt":"오늘 처음입니다. # Python program to swap two variables x = 5 y = 10 # To take inputs from the user #x = input('Enter value of x: ') #y = input('Enter value of y: ') # create a temporary variable and swap the values temp = x x =...","categories": ["coding"],
         "tags": [],
-        "url": "/first/",
+        "url": "/coding/first/",
         "teaser": null
       }]
